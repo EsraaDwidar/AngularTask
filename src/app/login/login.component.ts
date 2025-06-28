8a852ff5ad6f36c56ac5c6e6f5ec2c1887de5289
esra <esraadwidar770@gmail.com>
2025-06-28 22:19:39 +0300

    loginUpdates

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,8 +17,8 @@ export class LoginComponent implements OnInit {
   isLoading:boolean = false;
   
   loginForm:FormGroup = new FormGroup({
-    email: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(null,[Validators.required, Validators.pattern(/^[A-Z]/)])
+    username: new FormControl(null, [Validators.required]),
+    password: new FormControl(null,[Validators.required])
   });
   loginFormSubmit(loginForm:FormGroup){
     this.isLoading = true;
